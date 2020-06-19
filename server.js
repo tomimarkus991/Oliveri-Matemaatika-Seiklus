59b441f15a18e72521482b1e21756c9d3b774c00
tomimarkus991 <tomimarkus.alber@tptlive.ee>
2020-06-19 14:23:35 +0300

    try fix for heroku

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.static(__dirname + "/dist"));
 
 // send the user to index html page inspite of the url
-app.get("*", (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile(path.resolve(__dirname, "/dist/index.html"));
 });
 
