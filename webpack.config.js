4a85a25173a36ddd4b3bafb1bd02a963958ce9f5
tomimarkus991 <tomimarkus.alber@tptlive.ee>
2020-06-20 12:23:43 +0300

    try fix for heroku

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require("path");
 module.exports = {
   entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname + "dist/assets"),
+    path: path.resolve(__dirname, "dist/assets"),
     filename: "bundle.js",
   },
   devServer: {
