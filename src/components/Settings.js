4768a82ef9d1864be475883bb1f682b3a625c400
tomimarkus991 <tomimarkus.alber@tptlive.ee>
2020-06-24 21:19:02 +0300

    remove unneccessary css files

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import "../styles/Settings.css";
 
 class Settings extends Component {
   constructor() {
--- src/components/Snackbar.js
+++ src/components/Snackbar.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import "../styles/Snackbar.css";
 
 class Snackbar extends Component {
   constructor() {
