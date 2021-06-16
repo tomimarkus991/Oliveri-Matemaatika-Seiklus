8de56dd26d5e358c825dccc25abc9f56787936b7
tomimarkus991 <tomimarkusalber@gmail.com>
2021-06-16 16:34:15 +0300

    update numberinput to regular input

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -8,10 +8,14 @@ import {
   NumberDecrementStepper,
   HStack,
   calc,
+  Text,
+  Heading,
+  Input as ChakraInput,
 } from '@chakra-ui/react';
 import { evaluate } from 'mathjs';
 import { useContext, useEffect, useState } from 'react';
 import { HomeContext } from '../../context/homeContext';
+import { GetRandomNumber } from '../../utils/RandomNumber';
 
 const Input: React.FC = () => {
   const [value, setValue] = useState('');
@@ -19,6 +23,7 @@ const Input: React.FC = () => {
     firstNumber,
     operator,
     secondNumber,
+    minNumber,
     maxNumber,
     setFirstNumber,
     setSecondNumber,
@@ -26,30 +31,34 @@ const Input: React.FC = () => {
 
   const calculate = (e: any) => {
     e.preventDefault();
-    setFirstNumber(Math.floor(Math.random() * maxNumber));
-    setSecondNumber(Math.floor(Math.random() * maxNumber));
-
+    setFirstNumber(GetRandomNumber(minNumber, maxNumber));
+    setSecondNumber(GetRandomNumber(minNumber, maxNumber));
     if (evaluate(`${firstNumber} + ${secondNumber}`) === parseInt(value)) {
       console.log('correct');
     } else {
       console.log('wrong');
     }
+    e.target.reset();
   };
   return (
     <Box>
-      <HStack>
-        <Box>{firstNumber}</Box>
-        <Box>{operator}</Box>
-        <Box>{secondNumber}</Box>
+      <HStack justifyContent="center" mb={10}>
+        <Box>
+          <Heading isTruncated>{firstNumber}</Heading>
+        </Box>
+        <Box>
+          <Heading isTruncated>{operator}</Heading>
+        </Box>
+        <Box>
+          <Heading isTruncated>{secondNumber}</Heading>
+        </Box>
       </HStack>
       <form onSubmit={calculate}>
-        <NumberInput onChange={value => setValue(value)}>
-          <NumberInputField placeholder="Number" />
-          <NumberInputStepper>
-            <NumberIncrementStepper />
-            <NumberDecrementStepper />
-          </NumberInputStepper>
-        </NumberInput>
+        <ChakraInput
+          placeholder="Number"
+          type="number"
+          onChange={e => setValue(e.target.value)}
+        />
       </form>
     </Box>
   );
