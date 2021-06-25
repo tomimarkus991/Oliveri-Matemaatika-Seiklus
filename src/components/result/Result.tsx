5ce2a61bd09329d5b564625ad434287964f529c3
tomimarkus991 <tomimarkusalber@gmail.com>
2021-06-25 17:46:23 +0300

    add Progress bar

--- src/components/result/Result.tsx
+++ src/components/result/Result.tsx
@@ -1,48 +1,64 @@
-import { Flex, HStack, Spacer, Text } from '@chakra-ui/react';
+import {
+  Flex,
+  HStack,
+  Progress,
+  Spacer,
+  Text,
+  Box,
+} from '@chakra-ui/react';
 import { useContext } from 'react';
 import AnimatedNumber from 'react-animated-numbers';
 import { HomeContext } from '../../context/homeContext';
 
 const Result: React.FC = () => {
-  const { score, isCorrect, level } = useContext(HomeContext);
+  const { score, isCorrect, level, levelUpWhen } = useContext(HomeContext);
 
   return (
-    <Flex mb={6} justifyContent="center">
-      <HStack spacing="24px">
-        <Text fontSize={24}>Level</Text>
-        <AnimatedNumber
-          fontStyle={{
-            fontSize: 40,
-            fontWeight: 'bold',
-          }}
-          animateToNumber={level}
-          animationType={'random'}
-        />
-        <Spacer />
-        <Text fontSize={24}>Score</Text>
-        {isCorrect ? (
+    <Box m="auto">
+      <Progress
+        colorScheme="blue"
+        size="lg"
+        value={(score / levelUpWhen) * 100}
+        w={600}
+        borderRadius={20}
+      />
+      <Flex mb={6} justifyContent="center">
+        <HStack spacing="24px">
+          <Text fontSize={24}>Level</Text>
           <AnimatedNumber
             fontStyle={{
               fontSize: 40,
               fontWeight: 'bold',
-              color: '#38A169',
             }}
-            animateToNumber={score}
+            animateToNumber={level}
             animationType={'random'}
           />
-        ) : (
-          <AnimatedNumber
-            fontStyle={{
-              fontSize: 40,
-              fontWeight: 'bold',
-              color: '#E53E3E',
-            }}
-            animateToNumber={score}
-            animationType={'random'}
-          />
-        )}
-      </HStack>
-    </Flex>
+          <Spacer />
+          <Text fontSize={24}>Score</Text>
+          {isCorrect ? (
+            <AnimatedNumber
+              fontStyle={{
+                fontSize: 40,
+                fontWeight: 'bold',
+                color: '#38A169',
+              }}
+              animateToNumber={score}
+              animationType={'random'}
+            />
+          ) : (
+            <AnimatedNumber
+              fontStyle={{
+                fontSize: 40,
+                fontWeight: 'bold',
+                color: '#E53E3E',
+              }}
+              animateToNumber={score}
+              animationType={'random'}
+            />
+          )}
+        </HStack>
+      </Flex>
+    </Box>
   );
 };
 export default Result;
